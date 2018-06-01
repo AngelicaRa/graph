c6cffa251706db3af5545bd932d761be8fd8489f
pablo <pablo.martingu@gmail.com>
2018-06-01 20:02:15 +0000

    fast

---
+++ index.jsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Chart from "./components/Chart";
+
+ReactDOM.render(<Hello name='Lala'/>, document.getElementById("app"));
