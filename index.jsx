42db40cf276c853d100665123a5192e200e4d3f4
Pablo <pablo.martingu@gmail.com>
2018-06-03 12:09:52 +0100

    typescript

--- index.jsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import Chart from "./components/Chart";
-
-ReactDOM.render(<Hello name='Lala'/>, document.getElementById("app"));
---
+++ index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Chart from './src/components/Chart';
+
+ReactDOM.render(<Chart />, document.getElementById('app'));
